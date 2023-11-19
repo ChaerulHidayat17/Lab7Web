cff59694ffdf679fef07a4f7a4def59fcfb04d2d
ChaerulHidayat17 <chaerulhidayat70@gmail.com>
2023-11-19 21:02:45 +0700

    update README.md

---
+++ perulangan.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perulangan</title>
+</head>
+
+<body>
+    <!-- Perulangan For -->
+    <?php
+    echo "Perulangan 1 sampai 10 <br />";
+    for ($i = 1; $i <= 10; $i++) {
+        echo "Perulangan ke: " . $i . '<br />';
+    }
+    echo "Perulangan Menurun dari 10 ke 1 <br />";
+    for ($i = 10; $i >= 1; $i--) {
+        echo "Perulangan ke: " . $i . '<br />';
+    }
+    ?>
+
+    <!-- Perulangan While -->
+    <?php
+    echo "Perulangan 1 sampai 10 <br />";
+    $i = 1;
+    while ($i <= 10) {
+        echo "Perulangan ke: " . $i . '<br />';
+        $i++;
+    }
+    ?>
+
+    <!-- Perulangan dowhile -->
+    <?php
+    echo "Perulangan 1 sampai 10 <br />";
+    $i = 1;
+    do {
+        echo "Perulangan ke: " . $i . '<br />';
+        $i++;
+    } while ($i <= 10);
+    ?>
+</body>
+
+</html>
---
+++ php_dasar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP Dasar</title>
+</head>
+
+<body>
+    <!-- Belajar PHP Dasar -->
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+
+    <!-- Variable PHP -->
+    <?php
+    $nim = "0411500400";
+    $nama = 'Abdullah';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+
+
+</body>
+
+</html>
